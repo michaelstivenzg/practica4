0b2dede3757c687b015a1b96c08110d1f86f2e48
michaelstivenzg <80233659+michaelstivenzg@users.noreply.github.com>
2021-08-03 22:32:16 -0500

    commit 3

--- red.cpp
+++ red.cpp
@@ -13,12 +13,22 @@ void red::enrutadores(char nombre1, enrutador obj2)
 void red::impresion()
 {
    cout<<"Enlaces y costos: "<<endl;   
-    for(auto par=begin(enrutadorEnlace);par!=end(enrutadorEnlace);par++){
-        cout<<par->first<<'\t';
+    for(auto par:enrutadorEnlace){
+        cout<<par.first;
+        /*for(auto &&par2:par.second){
 
+        }*/
     }
 }
-/*for(auto it=enrutadorEnlace.begin();it!=enrutadorEnlace.end();it++){
+/*    for (auto var : mapa2) {
+        cout << "Clave mapa externo " << var.first << endl;
+        for (auto var1 : var.second) {
+            cout << "clave mapara interno "<< var1.first << " - valor ->" << var1.second << endl;
+        }
+    }
+ *
+ *
+ * for(auto it=enrutadorEnlace.begin();it!=enrutadorEnlace.end();it++){
     for(auto iter=it->second.guardarEnrutador();
 }*/
 /*    for(auto iteradorz=origen.begin();iteradorz!=origen.end();iteradorz++){
@@ -32,4 +42,5 @@ void red::impresion()
                 if(aux== iteradorz->first){
                     cout<<"Enrutador origen ["<<(*iteradorz).first<<"] enlazado al enrutador ["<<(*iteradorzz).first<<"] con costo ["<<(*iteradorzz).second<<"] "<<endl;
                 }
-}*/
+}
+*/
